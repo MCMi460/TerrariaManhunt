871d76976cddc9bc8cabdb35744760d82fa2a4c2
Deltaion Lee <32529306+MCMi460@users.noreply.github.com>
2024-05-02 21:05:57 -0500

    Team-based death markers & hide minimap deaths

---
+++ TerrariaManhunt.ILEditing.cs
@@ -0,0 +1,103 @@
+﻿using MonoMod.Cil;
+using System;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace TerrariaManhunt
+{
+    public partial class TerrariaManhunt : Mod
+    {
+        public static void HookDrawPlayerHead(ILContext il)
+        {
+            try
+            {
+                var c = new ILCursor(il);
+
+                c.GotoNext(i => i.MatchLdarg0());
+                var label = il.DefineLabel();
+
+                c.Emit(Mono.Cecil.Cil.OpCodes.Ldarg_2);
+                // Check if player is on the same team
+                c.EmitDelegate<Func<Player, bool>>(player => player.team != Main.CurrentPlayer.team);
+                // If not, go to label and continue function as normal
+                c.Emit(Mono.Cecil.Cil.OpCodes.Brfalse, label);
+                // Otherwise, prematurely end method
+                c.Emit(Mono.Cecil.Cil.OpCodes.Ret);
+
+                c.MarkLabel(label);
+            }
+            catch (Exception e)
+            {
+                MonoModHooks.DumpIL(ModContent.GetInstance<TerrariaManhunt>(), il);
+                throw new ILPatchFailureException(ModContent.GetInstance<TerrariaManhunt>(), il, e);
+            }
+        }
+
+        public static void HookDrawNPCHead(ILContext il)
+        {
+            try
+            {
+                var c = new ILCursor(il);
+
+                // Prematurely end method to prevent rendering NPC Map heads
+                c.Emit(Mono.Cecil.Cil.OpCodes.Ret);
+            }
+            catch (Exception e)
+            {
+                throw new ILPatchFailureException(ModContent.GetInstance<TerrariaManhunt>(), il, e);
+            }
+        }
+
+        /*
+         * Hides death markers on fullscreen map from players on other teams
+         * Note: Does not hide minimap TextureAssets.MapDeath
+         * See: HookDrawMinimapMarkers
+        */
+        public static void HookDrawDeathMarker(ILContext il)
+        {
+            try
+            {
+                var c = new ILCursor(il);
+
+                var label = il.DefineLabel();
+
+                c.Emit(Mono.Cecil.Cil.OpCodes.Ldarg_S, (byte)6);
+
+                // Check if player is on the same team
+                c.EmitDelegate<Func<int, bool>>(i => Main.player[i].team != Main.CurrentPlayer.team);
+                // If not, go to label and continue function as normal
+                c.Emit(Mono.Cecil.Cil.OpCodes.Brfalse, label);
+                // Otherwise, prematurely end method
+                c.Emit(Mono.Cecil.Cil.OpCodes.Ldc_I4, 0);
+                c.Emit(Mono.Cecil.Cil.OpCodes.Ret);
+
+                c.MarkLabel(label);
+            }
+            catch (Exception e)
+            {
+                throw new ILPatchFailureException(ModContent.GetInstance<TerrariaManhunt>(), il, e);
+            }
+        }
+
+        // Hides minimap icons, regardless of team
+        public static void HookDrawMinimapMarkers(ILContext il)
+        {
+            try
+            {
+                var c = new ILCursor(il);
+
+                c.GotoNext(i => i.MatchLdfld<Player>("showLastDeath"));
+                c.Index++;
+
+                // Pops the "showLastDeath" boolean from stack
+                c.Emit(Mono.Cecil.Cil.OpCodes.Pop);
+                // Replaces it with "false"
+                c.Emit(Mono.Cecil.Cil.OpCodes.Ldc_I4, 0);
+            }
+            catch (Exception e)
+            {
+                throw new ILPatchFailureException(ModContent.GetInstance<TerrariaManhunt>(), il, e);
+            }
+        }
+    }
+}
--- TerrariaManhunt.cs
+++ TerrariaManhunt.cs
@@ -1,8 +1,5 @@
-using MonoMod.Cil;
-using System;
 using Terraria;
 using Terraria.Graphics.Renderers;
-using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace TerrariaManhunt
@@ -14,63 +11,7 @@ namespace TerrariaManhunt
             IL_MapHeadRenderer.DrawPlayerHead += HookDrawPlayerHead;
             IL_NPCHeadRenderer.DrawWithOutlines += HookDrawNPCHead;
             IL_Main.DrawPlayerDeathMarker += HookDrawDeathMarker;
-        }
-
-        public static void HookDrawPlayerHead(ILContext il)
-        {
-            try
-            {
-                var c = new ILCursor(il);
-
-                c.GotoNext(i => i.MatchLdarg0());
-                var label = il.DefineLabel();
-
-                c.Emit(Mono.Cecil.Cil.OpCodes.Ldarg_2);
-                // Check if player is on the same team
-                c.EmitDelegate<Func<Player, bool>>(player => player.team != Main.CurrentPlayer.team);
-                // If not, go to label and continue function as normal
-                c.Emit(Mono.Cecil.Cil.OpCodes.Brfalse, label);
-                // Otherwise, prematurely end method
-                c.Emit(Mono.Cecil.Cil.OpCodes.Ret);
-
-                c.MarkLabel(label);
-            }
-            catch (Exception e)
-            {
-                MonoModHooks.DumpIL(ModContent.GetInstance<TerrariaManhunt>(), il);
-                throw new ILPatchFailureException(ModContent.GetInstance<TerrariaManhunt>(), il, e);
-            }
-        }
-
-        public static void HookDrawNPCHead(ILContext il)
-        {
-            try
-            {
-                var c = new ILCursor(il);
-
-                // Prematurely end method to prevent rendering NPC Map heads
-                c.Emit(Mono.Cecil.Cil.OpCodes.Ret);
-            }
-            catch (Exception e)
-            {
-                throw new ILPatchFailureException(ModContent.GetInstance<TerrariaManhunt>(), il, e);
-            }
-        }
-
-        // Note: Does not hide minimap TextureAssets.MapDeath ... mostly because I'm too lazy to care
-        public static void HookDrawDeathMarker(ILContext il)
-        {
-            try
-            {
-                var c = new ILCursor(il);
-
-                c.Emit(Mono.Cecil.Cil.OpCodes.Ldc_I4, 0);
-                c.Emit(Mono.Cecil.Cil.OpCodes.Ret);
-            }
-            catch (Exception e)
-            {
-                throw new ILPatchFailureException(ModContent.GetInstance<TerrariaManhunt>(), il, e);
-            }
+            IL_Main.DrawMap += HookDrawMinimapMarkers;
         }
     }
 }
